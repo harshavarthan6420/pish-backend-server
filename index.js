e840bfa230f6ab6043b39090269c2cf540756b27
harshavarthan6420 <harshavarthan6420@gmail.com>
2024-04-12 18:52:46 +0100

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PlaceModel = require("/models/Places.js");
+const PlaceModel = require("./models/Places");
 const cors = require("cors");
 
 app.use(express.json());
