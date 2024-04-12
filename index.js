fda724dfdf3c10aaef7b0c6e3b3389a52bc45df0
harshavarthan6420 <harshavarthan6420@gmail.com>
2024-04-12 18:58:04 +0100

    fixes

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(
-  cors()
+  cors({
+    origin:"https://pish.onrender.com/",
+    credentials:true
+  })
 );
 
 mongoose.connect(
